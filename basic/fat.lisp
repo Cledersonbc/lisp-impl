466baad51dff33238b454e55e5c04a3e470c89ec
cledersonbc <cledersonbc@live.com>
2018-07-08 13:56:09 -0300

    added factorial

---
+++ basic/fat.lisp
@@ -0,0 +1,12 @@
+#|
+Factorial of n is n of n = 0 or 1;
+Factorial n is n * (n - 1) if n > 1
+|#
+;; Author: Clederson Cruz
+
+(defun fat (n)
+  (if (< n 2)
+	  n
+	  (* n (fat (- n 1)))))
+
+(format t "Factorial of ~D is ~D.~%" (setq n (read)) (fat n))
